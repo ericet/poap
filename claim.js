293dc541f1f4b37a107421d7bc78c7aca1a686b8
Schen <schen@jerseymikes.com>
2021-09-23 15:10:17 -0400

    bulk claim

--- claim.js
+++ claim.js
@@ -28,55 +28,39 @@ let synchronous_request = function (url, params) {
 
 //claim poap by qr_hash
 async function main() {
-  if (process.argv.length < 3) {
-    console.log('argv: qr_hash address')
-    return
-  }
-
-  let a = process.argv[2].split('/')
-  let qr_hash = a[a.length - 1]
-
-  let url = claim_url + '?qr_hash=' + qr_hash
-  let o = await synchronous_request(url)
-  if (o.claimed == false) {
-    if (process.argv.length < 4) {
-      console.log('node claim qr_hash address')
-      return
-    }
-
-    let address = process.argv[3]
-    if (!address.startsWith('0x')) {
-      address = get_real_address(address)
-      if (address == undefined) {
-        console.log('bad account alias: ' + process.argv[3])
-        return
+  let addressList = getLines('address_list.txt');
+  let claimList = getLines('claim_list.txt');
+  let index =0;
+  for (let claim of claimList) {
+    let a = claim.split('/');
+    let qr_hash = a[a.length - 1];
+    let url = claim_url + '?qr_hash=' + qr_hash;
+    let o = await synchronous_request(url)
+    if (o.claimed == false) {
+      if(index>=addressList.length){
+        return;
       }
+      let body = await synchronous_request(claim_url, {
+        qr_hash: o.qr_hash,
+        address: addressList[index],
+        secret: o.secret,
+      })
+
+      // console.log(body)
+
+      console.log('\n* claimed for ' + addressList[index]);
+      index++;
+    } else {
+      console.log('\n' + o.beneficiary + ' had claimed this poap')
     }
-
-    let body = await synchronous_request(claim_url, {
-      qr_hash: o.qr_hash,
-      address: address,
-      secret: o.secret,
-    })
-
-    console.log(body)
-
-    console.log('\n* claimed for ' + process.argv[3])
-  } else {
-    console.log('\n' + o.beneficiary + ' had claimed this poap')
   }
+
 }
 
-function get_real_address(account_name) {
-  let list = fs.readFileSync('address_list.txt')
+function getLines(filename) {
+  let list = fs.readFileSync(filename)
   let lines = list.toString().split('\r\n')
-  for (let index in lines) {
-    let line = lines[index]
-    let arr = line.split(':')
-    if (arr[0] == account_name) return arr[1]
-  }
-
-  return undefined
+  return lines;
 }
 
 main()
